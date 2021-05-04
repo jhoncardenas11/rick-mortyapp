efc86875fe72711f26d9c1a08c958e327481101c
jhon cardenas <jhoncardenas.11@gmail.com>
2021-05-03 19:07:11 -0500

    home desktop v

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import { Link  } from 'react-router-dom';
 
+// styles
+import './styles/Navbar.scss';
+
+// media
+import logo from '../media/images/logo.png';
+
 class Navbar extends React.Component {
 
     render() {
 
         return(
             <div className="navbar">
-                <Link to="/">Home</Link>
-                <Link to="/all-characters">Characters</Link>
+                <img src={logo} alt="nav logo"/>
+                <div className="links">
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/all-characters">Characters</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         )
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+// styles
+import './styles/Home.scss';
+
+// media
+import homeLogo from '../media/images/homeLogo.png';
 
 class Home extends React.Component {
 
@@ -6,7 +13,13 @@ class Home extends React.Component {
 
         return(
             <div className="home">
-                <p>home</p>
+                <img src={homeLogo} alt="main logo"/>
+                <h1>Welcome to <br/> Rick and Morty app</h1>
+                <h2>To start please click in the next button</h2>
+                <div className="button">
+                    <Link className="link" to="/all-characters">start</Link>
+                </div>
+                <p>This app was made with React JS</p>
             </div>
         )
 
